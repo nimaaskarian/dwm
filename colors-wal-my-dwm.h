96a36a0c8c204915607831a3c5325c854603e137
nimaaskarian <nimaas1400@gmail.com>
2023-05-10 13:56:40 +0330

    added pywal colors

---
+++ colors-wal-my-dwm.h
@@ -0,0 +1,12 @@
+static const char norm_fg[] = "#c9bdbc";
+static const char norm_bg[] = "#090E12";
+static const char norm_border[] = "#8A7F80";
+
+static const char sel_fg[] = "#090E12";
+static const char sel_bg[] = "#8D3A3C";
+static const char sel_border[] = "#8D3A3C";
+
+static const char inact_fg[] = "#090E12";
+static const char inact_bg[] = "#6E7174";
+static const char inact_border[] = "#6E7174";
+
